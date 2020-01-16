274f97eff8a31807913d274a185ab5ad04f5e96e
keith <xqk559@gmail.com>
2020-01-16 12:24:57 -0500

    Before Ejection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from './Components/Layouts/Layout' ;
+import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder' ;
+import classes from './Components/Layouts/Layout.css' ;
 
 function App() {
   return (
     <div>
      <Layout>
-       <p>TEST</p>
+       <BurgerBuilder />
      </Layout>
     </div>
   );
--- src/Components/Layouts/Layout.js
+++ src/Components/Layouts/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react' ;
 import ReactAux from '../../hoc/ReactAux' ;
+import classes from './Layout.css' ;
 
 const layout = ( props ) => (
-    <ReactAux>
-    <div>
+    <ReactAux className={classes.Content} >
+    <div className={classes.Content}>
         Toolbar, SideDrawer, Backdrop
     </div>
-    <main>
+    <main className={classes.Content}>
         {props.children}
     </main>
     </ReactAux>
